86edef9be5c53fb9c526376af8c694b87f2461e3
Charalampos Lantzos <xaris.lantz@gmail.com>
2024-03-14 22:55:23 +0100

    Initial commit

---
+++ banktransaction-microservice/src/main/java/com/banktransaction/banktransactionmicroservice/BanktransactionMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.banktransaction.banktransactionmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanktransactionMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanktransactionMicroserviceApplication.class, args);
+	}
+
+}
---
+++ banktransaction-microservice/src/test/java/com/banktransaction/banktransactionmicroservice/BanktransactionMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banktransaction.banktransactionmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanktransactionMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
